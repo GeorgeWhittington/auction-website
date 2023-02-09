e1ec889ae59410cd96d1a0a67e9dc1e2d4f3376d
Daniel Taylor <daktaylor98@gmail.com>
2023-02-09 16:36:56 +0000

    added basic models for items & sets

--- jamhouse/shop/models.py
+++ jamhouse/shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Set(models.Model):
+    set_desc = models.CharField(max_length=128)
+    set_price = models.DecimalField(decimal_places=2, max_digits=10)
+    
+class Item(models.Model):
+    item_desc = models.CharField(max_length=128)
+    item_price = models.DecimalField(decimal_places=2, max_digits=10)
+    item_sold = models.DateTimeField(null=True)
+    item_sets = models.ManyToManyField(Set)
+
+
+
