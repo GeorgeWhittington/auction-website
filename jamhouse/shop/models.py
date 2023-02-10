24418e3fbbedfe5e51023998f5e9a989b3f6bb2e
George Whittington <george2.whittington@live.uwe.ac.uk>
2023-02-10 12:14:10 +0000

    Configured admin site for Item+Set models. Renamed some columns + made m2m relationship editable on both sides.

--- jamhouse/shop/models.py
+++ jamhouse/shop/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
 class Set(models.Model):
-    set_desc = models.CharField(max_length=128)
-    set_price = models.DecimalField(decimal_places=2, max_digits=10)
-    
+    description = models.CharField(max_length=128)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    items = models.ManyToManyField("Item", through="Item_sets", blank=True)
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        verbose_name_plural = "Sets"
+
+
 class Item(models.Model):
-    item_desc = models.CharField(max_length=128)
-    item_price = models.DecimalField(decimal_places=2, max_digits=10)
-    item_sold = models.DateTimeField(null=True, blank=True)
-    item_sets = models.ManyToManyField(Set)
+    description = models.CharField(max_length=128)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    sold = models.DateTimeField(null=True, blank=True)
+    sets = models.ManyToManyField("Set", blank=True)
+
+    def __str__(self):
+        return self.description
