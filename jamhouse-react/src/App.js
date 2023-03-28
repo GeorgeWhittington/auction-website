cd7adce834a90a2276bd6a1543ef56da510cd0f7
George Whittington <george2.whittington@live.uwe.ac.uk>
2023-03-28 17:40:34 +0100

    Added preliminary html and started styling

--- jamhouse-react/src/App.js
+++ jamhouse-react/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 import Home from "./components/Home";
 import Search from "./components/Search";
@@ -8,10 +9,28 @@ import Set from "./components/Set";
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/search">Search</Link>
-      </nav>
+      <header>
+        <div id="upper-header">
+          <img id="logo" src="./jam-house-logo.png"/>
+          <div id="login-options">
+            {/* Split the login/register vs welcome {username}!/logout section into a component */}
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+          </div>
+          <div id="search-box">
+            <input type="text" placeholder="Search"></input>
+          </div>
+        </div>
+        <nav id="lower-header">
+          <a href="#">About Us</a>
+          <a href="#">Locations</a>
+          <a href="#">Recently Sold</a>
+          <a href="#">Contact Us</a>
+
+          {/* <Link to="/">Home</Link>
+          <Link to="/search">Search</Link> */}
+        </nav>
+      </header>
 
       <Routes>
         <Route path="/" element={<Home />} />
