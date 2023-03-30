22e1f7e978da834bf1f2de8c8819e9903ca53b9f
George Whittington <george2.whittington@live.uwe.ac.uk>
2023-03-30 04:07:30 +0100

    Added fontawesome, moved login menu into component, began styling page

--- jamhouse-react/src/App.js
+++ jamhouse-react/src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { icon } from "@fortawesome/fontawesome-svg-core/import.macro"
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 
 import Home from "./components/Home";
@@ -6,19 +9,34 @@ import Search from "./components/Search";
 import Item from "./components/Item";
 import Set from "./components/Set";
 
+function LoginLogoutRegister({ username }) {
+  if (username !== null) {
+    var options = <><a href="#">Welcome {username}!</a><a href="#">Logout</a></>;
+  } else {
+    var options = <><a href="#">Login</a><a href="#">Register</a></>;
+  }
+
+  return (
+    <div id="login-options">
+      {options}
+    </div>
+  )
+}
+
 function App() {
+  let username = null;
+
   return (
     <Router>
       <header>
         <div id="upper-header">
           <img id="logo" src="./jam-house-logo.png"/>
-          <div id="login-options">
-            {/* Split the login/register vs welcome {username}!/logout section into a component */}
-            <a href="#">Login</a>
-            <a href="#">Register</a>
-          </div>
-          <div id="search-box">
-            <input type="text" placeholder="Search"></input>
+          <div id="header-right">
+            <LoginLogoutRegister username={username} />
+            <div id="search-box">
+              <input type="text" placeholder="Search"></input>
+              <button><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+            </div>
           </div>
         </div>
         <nav id="lower-header">
@@ -31,21 +49,22 @@ function App() {
           <Link to="/search">Search</Link> */}
         </nav>
       </header>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/item/:id" element={<Item />} />
-        <Route path="/set/:id" element={<Set />} />
-        {/* Also need to have:
-        - Basket
-        - Checkout workflow
-        - Login
-        - Logout
-        - User Profile Page (displaying prev orders/account details)
-        - View Repository
-        */}
-      </Routes>
+      <div id="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/item/:id" element={<Item />} />
+          <Route path="/set/:id" element={<Set />} />
+          {/* Also need to have:
+          - Basket
+          - Checkout workflow
+          - Login
+          - Logout
+          - User Profile Page (displaying prev orders/account details)
+          - View Repository
+          */}
+        </Routes>
+      </div>
     </Router>
   );
 }
