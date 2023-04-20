783824ddc9e31bfe719a1b14dda29a3e7e2e8a43
George Whittington <george2.whittington@live.uwe.ac.uk>
2023-04-20 21:09:06 +0100

    Created repair set function, added admin action to use it

--- jamhouse/shop/models.py
+++ jamhouse/shop/models.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 class Set(models.Model):
     description = models.CharField(max_length=128)
     price = models.DecimalField(decimal_places=2, max_digits=10)
+    archived = models.BooleanField(default=False)
     items = models.ManyToManyField("Item", through="Item_sets", blank=True)
 
     def __str__(self):
@@ -14,6 +15,48 @@ class Set(models.Model):
 
     def sold(self):
         return bool(self.items.filter(sold_at__isnull=False).count())
+
+    def create_replacement(self):
+        if self.archived:
+            # A replacement set has already been created
+            return
+
+        if not self.sold():
+            # don't need to create a replacement set if this one isn't sold
+            return
+
+        print(f"replacing items in set {self.id}")
+
+        sold_qs = self.items.filter(sold_at__isnull=False).all()
+        unsold_qs = self.items.filter(sold_at__isnull=True).all()
+
+        replacement_items = []
+
+        for item in sold_qs:
+            candidate_qs = Item.objects.filter(sold_at__isnull=True, description=item.description)
+            candidate_qs = candidate_qs.exclude(id__in=[i.id for i in replacement_items])
+            candidate_qs = candidate_qs.exclude(id__in=[i.id for i in unsold_qs]).all()
+
+            print(f"set {self.id}: candidate replacements {candidate_qs}")
+
+            if len(candidate_qs) == 0:
+                # Can't replace an item in the set
+                return
+
+            replacement_items.append(candidate_qs[0])
+
+        new_set = Set.objects.create(
+            description=self.description,
+            price=self.price)
+
+        new_set.save()
+        new_set.items.set(list(list(unsold_qs) + replacement_items))
+        new_set.save()
+
+        self.archived = True
+        self.save()
+
+        return new_set
 
 
 class Item(models.Model):
