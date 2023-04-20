9ef62350b60f463e0617b261a5527826352bed42
Daniel Taylor <daktaylor98@gmail.com>
2023-04-20 19:13:32 +0100

    Added enum for checkout status as a constant

--- jamhouse-react/src/constants.js
+++ jamhouse-react/src/constants.js
@@ -3,4 +3,10 @@ const hours24 = 24 * 60 * 60;
 const numberRegex = /^\d+$/;
 const currentYear = new Date().getFullYear();
 
-export { api, hours24, numberRegex, currentYear };
+const checkoutStatus = {
+    "SUCCESS":0,
+    "ITEMS_ALREADY_PURCHASED":1,
+    "SETS_PURCHASED":2
+}
+
+export { api, hours24, numberRegex, currentYear, checkoutStatus };
